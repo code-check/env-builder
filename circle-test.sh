bd7b620abc3cff7fc7e196b7ce112e0472f61f34
takayukioda <takayuki@odashi.me>
2015-12-18 10:50:46 +0900

    Update variable name

--- circle-test.sh
+++ circle-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
-docker build -t all -f docker/all/Dockerfile --build-arg branch=${CIRCLE_BRANCH} docker/all
+docker build --build-arg CLONE_BRANCH=${CIRCLE_BRANCH} -t all -f docker/all/Dockerfile docker/all
 kill %1
