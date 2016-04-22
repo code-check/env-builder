fefff8b732c3513e1b41b643994760f95069f915
shok4a <shok4a@users.noreply.github.com>
2016-04-22 13:32:51 +0900

    Fix groovy

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 (while true; do echo "Don't DIE!!!!"; sleep 60; done) & # jobs %1
 docker build -t all -f docker/all/Dockerfile docker/all
 kill %1
+
+docker run -it all bash groovy
+docker run -it all bash --login -c 'groovy'
