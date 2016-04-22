511d5dc08615b98a4cc1276c43f61194452477eb
shok4a <shok4a@users.noreply.github.com>
2016-04-22 18:04:13 +0900

    Fix node

--- test.sh
+++ test.sh
@@ -3,7 +3,19 @@
 docker build -t all -f docker/all/Dockerfile docker/all
 kill %1
 
-docker run -it all bash groovy
-docker run -it all bash --login -c 'groovy'
-docker run -it all bash gradle
-docker run -it all bash --login -c 'gradle'
+docker run --rm -it all which groovy
+docker run --rm -it all groovy --version
+docker run --rm -it all bash --login -c 'which groovy'
+docker run --rm -it all bash --login -c 'groovy --version'
+docker run --rm -it all which gradle
+docker run --rm -it all gradle --version
+docker run --rm -it all bash --login -c 'which gradle'
+docker run --rm -it all bash --login -c 'gradle --version'
+docker run --rm -it all which node
+docker run --rm -it all node --version
+docker run --rm -it all bash --login -c 'which node'
+docker run --rm -it all bash --login -c 'node --version'
+docker run --rm -it all which codecheck
+docker run --rm -it all codecheck
+docker run --rm -it all bash --login -c 'which codecheck'
+docker run --rm -it all bash --login -c 'codecheck'
